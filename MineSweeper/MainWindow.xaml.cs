f932ed93dc9b33684f8a737b29d1f15794144cbb
WonyoungPark <elinpark.102@gmail.com>
2022-02-04 17:19:52 +0900

    옵션버튼기능구현

---
+++ MineSweeper/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MineSweeper
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Option_Button(object sender, RoutedEventArgs e)
+        {
+            Option option = new Option();
+
+            option.Show();
+        }
+
+        private void Start_Button(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MineSweeper/Option.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MineSweeper
+{
+    /// <summary>
+    /// Option.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Option : Window
+    {
+        public Option()
+        {
+            InitializeComponent();
+        }
+
+        private void Okay_Button(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("적용되었습니다.");
+
+            MainWindow mainWindow = new MainWindow();
+        }
+    }
+}
